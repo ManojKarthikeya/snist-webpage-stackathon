e232591170f753189f324133fb50c626d5bd9df0
ManojKarthikeya <manojkarthikeya@gmail.com>
2022-09-08 12:27:41 +0530

    login page updated

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./header-styles.css";
+import logo from "./../../Assets/sreenidhi-logo.png";
 
 export default function Header() {
-	return (
-		<div className="header">
-			<Link to='/'>SNIST</Link>
-			<div className="headertabs">
-                <Link to="/departments">Departments</Link>
-                <Link to="/campus">Campus</Link>
-                <Link to="/events">Events</Link>
-                <Link to="/gallery">Gallery</Link>
-                <Link to="/login">Login</Link>
-            </div>
-		</div>
-	);
+  return (
+    <div className="header">
+      <Link to="/">
+        <img className="snistlogo" src={logo} alt="SNIST" />
+      </Link>
+      <div className="headertabs">
+        <Link className='headertab' to="/departments">Departments</Link>
+        <Link className='headertab' to="/campus">Campus</Link>
+        <Link className='headertab' to="/events">Events</Link>
+        <Link className='headertab' to="/gallery">Gallery</Link>
+        <Link className='headertab' to="/login">Login</Link>
+      </div>
+    </div>
+  );
 }

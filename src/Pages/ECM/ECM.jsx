359d147f928d7071bbc633f9aa9e795e1aa56829
ManojKarthikeya <manojkarthikeya@gmail.com>
2022-09-08 09:51:32 +0530

    added department pages

---
+++ src/Pages/ECM/ECM.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './ecm-styles.css'
+
+function ECM(){
+    return(
+        <div className='ECMpagemain'>ECM Page</div>
+    )
+}
+
+export default ECM;
---
+++ src/Pages/ITdept/ITdept.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './it-styles.css'
+
+export default function ITdept() {
+  return (
+    <div>ITdept</div>
+  )
+}

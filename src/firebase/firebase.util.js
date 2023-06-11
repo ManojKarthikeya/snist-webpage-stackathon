bcdaacc8778166055bc9197e66318a3fe82be484
Manoj Karthikeya <61418022+ManojKarthikeya@users.noreply.github.com>
2023-06-12 01:18:16 +0530

    Update firebase.util.js

--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -2,13 +2,12 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyCaYHLwAaNPWa8XTlF-i9SvC7Mk_OAYCVE",
-    authDomain: "snist-stackathon.firebaseapp.com",
-    projectId: "snist-stackathon",
-    storageBucket: "snist-stackathon.appspot.com",
-    messagingSenderId: "662946027917",
-    appId: "1:662946027917:web:081ee77b6c9191b2dd85c4",
-    measurementId: "G-WN8H3G06SS"
+	    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	    appId: process.env.REACT_APP_FIREBASE_APP_ID
   });
 
   export const auth = app.auth()
